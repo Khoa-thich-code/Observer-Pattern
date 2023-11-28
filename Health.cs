9d27e681351140ccbf7d30b1a45cb08a10a4e8c9
Khoa <130851064+Khoa-thich-code@users.noreply.github.com>
2023-11-28 13:46:28 +0700

    Create Health.cs

---
+++ Health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float fullHealth = 100f;
+    [SerializeField] private float drainPerSecond = 2f;
+    float currentHealth = 0;
+
+    private void Awake()
+    {
+        ResetHealth();
+        StartCoroutine(HealthDrain());
+    }
+
+    public float GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = fullHealth;
+    }
+
+    private IEnumerator HealthDrain()
+    {
+        while (currentHealth > 0)
+        {
+            currentHealth -= drainPerSecond;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
